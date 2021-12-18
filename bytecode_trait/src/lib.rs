08c5c8c1f3447fbafeea5394c34453212193b17c
Yashodhan Joshi <yjdoc2@gmail.com>
2021-12-18 13:57:56 +0530

    Change primitive type bytecode impl to us native methods Remove usize impl

--- bytecode_trait/src/lib.rs
+++ bytecode_trait/src/lib.rs
@@ -1,3 +1,4 @@
+use std::convert::TryFrom;
 pub enum BytecodeError {
     EmptyInstruction,
     InvalidInstruction,
@@ -16,118 +17,99 @@ pub trait Bytecodable: Sized {
 
 impl Bytecodable for u8 {
     fn compile(&self) -> Vec<u8> {
-        return vec![*self];
+        Vec::from(self.to_le_bytes())
     }
     fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
         if bytes.len() < 1 {
             return Err(BytecodeError::InvalidInstruction);
         }
-        Ok((bytes[0], 1))
+        let bytes: [u8; 1] = [bytes[0]];
+        Ok((u8::from_le_bytes(bytes), 1))
     }
 }
 
 impl Bytecodable for u16 {
     fn compile(&self) -> Vec<u8> {
-        let lower = *self as u8;
-        let higher = (*self >> 8) as u8;
-        return vec![lower, higher];
+        Vec::from(self.to_le_bytes())
     }
     fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
         if bytes.len() < 2 {
             return Err(BytecodeError::InvalidInstruction);
         }
-        let lower = bytes[0] as u16;
-        let higher = bytes[1] as u16;
-        Ok((higher << 8 | lower, 2))
+        let bytes: [u8; 2] = <[u8; 2]>::try_from(&bytes[0..2]).unwrap();
+        Ok((u16::from_le_bytes(bytes), 2))
     }
 }
 
 impl Bytecodable for u32 {
     fn compile(&self) -> Vec<u8> {
-        let lower1 = *self as u8;
-        let lower2 = (*self >> 8) as u8;
-        let higher1 = (*self >> 16) as u8;
-        let higher2 = (*self >> 24) as u8;
-        return vec![lower1, lower2, higher1, higher2];
+        Vec::from(self.to_le_bytes())
     }
     fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
         if bytes.len() < 4 {
             return Err(BytecodeError::InvalidInstruction);
         }
-        let lower1 = bytes[0] as u32;
-        let lower2 = (bytes[1] as u32) << 8;
-        let higher1 = (bytes[2] as u32) << 16;
-        let higher2 = (bytes[3] as u32) << 24;
-        Ok((lower1 | lower2 | higher1 | higher2, 4))
+        let bytes: [u8; 4] = <[u8; 4]>::try_from(&bytes[0..4]).unwrap();
+        Ok((u32::from_le_bytes(bytes), 4))
     }
 }
 
 impl Bytecodable for u64 {
     fn compile(&self) -> Vec<u8> {
-        let lower1 = *self as u8;
-        let lower2 = (*self >> 8) as u8;
-        let lower3 = (*self >> 16) as u8;
-        let lower4 = (*self >> 24) as u8;
-        let higher1 = (*self >> 32) as u8;
-        let higher2 = (*self >> 40) as u8;
-        let higher3 = (*self >> 48) as u8;
-        let higher4 = (*self >> 56) as u8;
-        return vec![
-            lower1, lower2, lower3, lower4, higher1, higher2, higher3, higher4,
-        ];
+        Vec::from(self.to_le_bytes())
     }
     fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
         if bytes.len() < 8 {
             return Err(BytecodeError::InvalidInstruction);
         }
-        let lower1 = bytes[0] as u64;
-        let lower2 = (bytes[1] as u64) << 8;
-        let lower3 = (bytes[2] as u64) << 16;
-        let lower4 = (bytes[3] as u64) << 24;
-        let higher1 = (bytes[0] as u64) << 32;
-        let higher2 = (bytes[1] as u64) << 40;
-        let higher3 = (bytes[2] as u64) << 48;
-        let higher4 = (bytes[3] as u64) << 56;
-        Ok((
-            lower1 | lower2 | lower3 | lower4 | higher1 | higher2 | higher3 | higher4,
-            8,
-        ))
+        let bytes: [u8; 8] = <[u8; 8]>::try_from(&bytes[0..8]).unwrap();
+        Ok((u64::from_le_bytes(bytes), 8))
     }
 }
 
 impl Bytecodable for i8 {
     fn compile(&self) -> Vec<u8> {
-        (*self as u8).compile()
+        Vec::from(self.to_le_bytes())
     }
     fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
-        u8::parse(bytes).map(|(x, s)| (x as i8, s))
+        if bytes.len() < 1 {
+            return Err(BytecodeError::InvalidInstruction);
+        }
+        let bytes: [u8; 1] = [bytes[0]];
+        Ok((i8::from_le_bytes(bytes), 1))
     }
 }
 
 impl Bytecodable for i16 {
     fn compile(&self) -> Vec<u8> {
-        (*self as u16).compile()
+        Vec::from(self.to_le_bytes())
     }
     fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
-        u16::parse(bytes).map(|(x, s)| (x as i16, s))
+        if bytes.len() < 2 {
+            return Err(BytecodeError::InvalidInstruction);
+        }
+        let bytes: [u8; 2] = <[u8; 2]>::try_from(&bytes[0..2]).unwrap();
+        Ok((i16::from_le_bytes(bytes), 2))
     }
 }
 
 impl Bytecodable for i32 {
     fn compile(&self) -> Vec<u8> {
-        (*self as u32).compile()
+        Vec::from(self.to_le_bytes())
     }
     fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
-        u32::parse(bytes).map(|(x, s)| (x as i32, s))
+        let bytes: [u8; 4] = <[u8; 4]>::try_from(&bytes[0..4]).unwrap();
+        Ok((i32::from_le_bytes(bytes), 4))
     }
 }
 
 impl Bytecodable for i64 {
     fn compile(&self) -> Vec<u8> {
-        (*self as u64).compile()
+        Vec::from(self.to_le_bytes())
     }
     fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
-        u64::parse(bytes).map(|(x, s)| (x as i64, s))
+        let bytes: [u8; 8] = <[u8; 8]>::try_from(&bytes[0..8]).unwrap();
+        Ok((i64::from_le_bytes(bytes), 8))
     }
 }
 
@@ -139,45 +121,5 @@ impl Bytecodable for bool {
     // C style truth/false value
     fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
         u8::parse(bytes).map(|(x, s)| (x != 0, s))
-    }
-}
-
-// using usize should be avoided as the compiled bytecode will not be portable across arch
-// fixed size type should be used instead, These implementations are given as to
-// complete all numerical primitive types
-
-#[cfg(target_pointer_width = "16")]
-impl Bytecodable for usize {
-    fn compile(&self) -> Vec<u8> {
-        (*self as u16).compile()
-    }
-    // this is dicey, as this essentially mimics
-    // C style truth/false value
-    fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
-        u6::parse(bytes).map(|(x, s)| (x as usize, s))
-    }
-}
-
-#[cfg(target_pointer_width = "32")]
-impl Bytecodable for usize {
-    fn compile(&self) -> Vec<u8> {
-        (*self as u32).compile()
-    }
-    // this is dicey, as this essentially mimics
-    // C style truth/false value
-    fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
-        u32::parse(bytes).map(|(x, s)| (x as usize, s))
-    }
-}
-
-#[cfg(target_pointer_width = "64")]
-impl Bytecodable for usize {
-    fn compile(&self) -> Vec<u8> {
-        (*self as u64).compile()
-    }
-    // this is dicey, as this essentially mimics
-    // C style truth/false value
-    fn parse(bytes: &[u8]) -> Result<(Self, usize), BytecodeError> {
-        u64::parse(bytes).map(|(x, s)| (x as usize, s))
     }
 }
